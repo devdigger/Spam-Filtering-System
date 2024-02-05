103b9d123badf3beaf2f487e928b717009a1f1f1
Sagar Kumar <aec.cse.sagarkumar@gmail.com>
2024-02-05 22:08:31 +0530

    Update 2_📊_Model Testing.py

--- pages/2_📊_Model Testing.py
+++ pages/2_📊_Model Testing.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 
+
 if 'model' not in st.session_state:
     st.subheader("Please Train model first before continuing....")
 else:
@@ -16,41 +17,59 @@ else:
     st.header("Enter email text below : ")
 
     msg = st.text_area('Type or paste email...',height=200)
+    import streamlit as st
 
-    if msg:
+    # Create a button to clear the page
+    sub_button = st.button("Submit")
+    if sub_button not in st.session_state:
+        st.session_state['sub_button'] = True
+    # Check if the button is clicked
+
+    if sub_button or msg:
+        
         prediction = email_classifier.test_single_mail(str(msg))
         print(prediction)
 
-        st.write("The email is predicted as : ")
+        placeholder = st.empty()
+        with placeholder.container():
+            st.write("The email is predicted as : ")
 
-        if prediction==1:
-            st.error("Spam")
-        else:
-            st.success("Non-Spam")
+            if prediction==1:
+                st.error("Spam")
+            else:
+                st.success("Non-Spam")
 
-        display = st.subheader("Was this email Correctly classfied?")
-        container1 = st.empty()
-        container2 = st.empty()
-        # Add two checkboxes to the container
-        yes = container1.checkbox("Yes")
-        no = container2.checkbox("No")
-        clicked = False
-        if yes:
-            st.success("Thanks for your feedback.")
-            clicked = True
-        if no:
-            result = "Spam" if prediction == 0 else "Non-Spam"
-            prediction = 0 if 1 else 1
-            st.success(f"Thanks for your feedback.This is now marked as {result}")
-            clicked = True
-        if clicked:
-            container1.empty()
-            container2.empty()
 
-            with st.spinner("Updating and Saving Model..."):
-                email_classifier.update_model(prediction,msg)
+            st.subheader("Was this email Correctly classfied?")
             
-            st.info("Model has been saved with new data points")
+            box1 = st.empty()
+            box2 = st.empty()
+            # Add two checkboxes to the container
+            yes = box1.button("  Yes   ")
+            no = box2.button("   No   ")
+            clicked = False
+            if yes:
+                st.success("Thanks for your feedback.")
+                clicked = True
+            if no:
+                result = "Spam" if prediction == 0 else "Non-Spam"
+                prediction = 0 if prediction == 1 else 1
+                print("-----------Prediction=",prediction)
+                st.success(f"Thanks for your feedback.This is now marked as {result}")
+                clicked = True
+            if clicked:
+                print("---------------alrady clicked here--------------")
+
+                box1.empty()
+                box2.empty()
+                clicked = False
+
+                with st.spinner("Updating and Saving Model..."):
+                    email_classifier.update_model(prediction,msg)
+                
+                info_msg = st.info("Model has been saved with new data points")
+        
+                
 
             
         
